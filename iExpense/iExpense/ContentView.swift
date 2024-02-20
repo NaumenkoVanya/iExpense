c4e522654b043896b201dbcec9bb034c86fa4dc1
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-20 13:12:23 +0300

    add Showing and hiding views

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,31 +8,56 @@
 import SwiftUI
 import Observation
 
-@Observable class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    var body: some View {
+        Text("Hello, \(name)")
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
 }
+
+//@Observable class User {
+//    var firstName = "Bilbo"
+//    var lastName = "Baggins"
+//}
+
 struct ContentView: View {
-    @State private var user = User()
+    @State private var showingSheet = false
+    @State private var myName = ""
+//    @State private var user = User()
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-                .font(.title)
-                .foregroundStyle(.white)
-            Section {
-                TextField("First naem", text: $user.firstName)
-                TextField("Last name", text: $user.lastName)
-            }
+        TextField("Name", text: $myName)
             .padding()
-            .bold()
             .textFieldStyle(.roundedBorder)
-            
-          
-            
+    
+        Button("Shoe Sheet") {
+            showingSheet.toggle()
         }
-        .padding()
-        .background(.gray)
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "\(myName)")
+        }
+//        VStack {
+//            Text("Your name is \(user.firstName) \(user.lastName)")
+//                .font(.title)
+//                .foregroundStyle(.white)
+//            Section {
+//                TextField("First naem", text: $user.firstName)
+//                TextField("Last name", text: $user.lastName)
+//            }
+//            .padding()
+//            .bold()
+//            .textFieldStyle(.roundedBorder)
+//            
+//          
+//            
+//        }
+//        .padding()
+//        .background(.gray)
     }
     
 }
