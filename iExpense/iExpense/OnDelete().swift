c583187ff0fd6e7c9524374e798a20cd0a30ea9a
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-20 13:31:55 +0300

    add Button redact

---
+++ iExpense/iExpense/OnDelete().swift
@@ -0,0 +1,42 @@
+//
+//  OnDelete().swift
+//  iExpense
+//
+//  Created by Ваня Науменко on 20.02.24.
+//
+
+import SwiftUI
+
+struct OnDelete__: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+}
+
+#Preview {
+    OnDelete__()
+}
